f5976d600faa5c21fa27ff9bc83b3f28bfb095f2
Paul Gschwendtner <paulgschwendtner@gmail.com>
2021-06-11 14:35:31 +0200

    Init

---
+++ new/extra/ks-Arab.mjs
@@ -0,0 +1,10 @@
+/**
+ * @license
+ * Copyright Google LLC All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+const u = undefined;
+export default [];
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoia3MtQXJhYi5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbIi4uLy4uLy4uLy4uLy4uLy4uLy4uL3BhY2thZ2VzL2NvbW1vbi9sb2NhbGVzL2V4dHJhL2tzLUFyYWIudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7Ozs7OztHQU1HO0FBQ0gsTUFBTSxDQUFDLEdBQUcsU0FBUyxDQUFDO0FBRXBCLGVBQWUsRUFBRSxDQUFDIiwic291cmNlc0NvbnRlbnQiOlsiLyoqXG4gKiBAbGljZW5zZVxuICogQ29weXJpZ2h0IEdvb2dsZSBMTEMgQWxsIFJpZ2h0cyBSZXNlcnZlZC5cbiAqXG4gKiBVc2Ugb2YgdGhpcyBzb3VyY2UgY29kZSBpcyBnb3Zlcm5lZCBieSBhbiBNSVQtc3R5bGUgbGljZW5zZSB0aGF0IGNhbiBiZVxuICogZm91bmQgaW4gdGhlIExJQ0VOU0UgZmlsZSBhdCBodHRwczovL2FuZ3VsYXIuaW8vbGljZW5zZVxuICovXG5jb25zdCB1ID0gdW5kZWZpbmVkO1xuXG5leHBvcnQgZGVmYXVsdCBbXTtcbiJdfQ==
---
+++ new/package.publish.sh
@@ -0,0 +1,2 @@
+
+"C:/users/paul/_bazel_paul/gsqtgr5s/external/nodejs_windows_amd64/bin/node.cmd" "C:/users/paul/_bazel_paul/gsqtgr5s/external/nodejs_windows_amd64/bin/nodejs/node_modules/npm/bin/npm-cli.js" publish "C:\users\paul\_bazel_paul\gsqtgr5s\execroot\angular/bazel-out/x64_windows-fastbuild/bin/packages/common/locales/package" "$@"
